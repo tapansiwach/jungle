8f00613c08bfdeedf1e030b3c4f19baf1ab28117
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-14 04:24:12 -0400

    add routes for login /  user session

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
