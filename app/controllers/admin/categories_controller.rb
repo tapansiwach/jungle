c5c46979e9b13016bcc53ed98e21d095eae103fd
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-14 02:23:21 -0400

    create template & actions to create a new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,13 @@
 class Admin::CategoriesController < ApplicationController
+
   def index
     @categories = Category.order(name: :asc).all
   end
+
+  def new
+  end
+  
+  def create
+  end
+  
 end
