cb086f85b8074090009fc867e5b0d89f6ec64d49
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-18 19:38:22 -0400

    validate that name, email, password & password_confirmation fields are provided when creating a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,52 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "requires an email field" 
+  it "requires an email field" do
+    user = User.new({
+      # email: "name@example.com",
+      name: "Mowgli",
+      password: "junglebook",
+      password_confirmation: "junglebook"
+    })
+    expect(user).to_not be_valid
+  end
   it "ensures that the email is unique, isn't already in the database" 
-  it "requires a name field" 
-  it "requires a password field" 
-  it "requires a password_confirmation field" 
+  it "requires a name field" do
+    user = User.new({
+      email: "name@example.com",
+      # name: "Mowgli",
+      password: "junglebook",
+      password_confirmation: "junglebook"
+    })
+    expect(user).to_not be_valid
+  end
+  it "requires a password field" do
+    user = User.new({
+      email: "name@example.com",
+      name: "Mowgli",
+      # password: "junglebook",
+      password_confirmation: "junglebook"
+    })
+    expect(user).to_not be_valid
+  end
+  it "requires a password_confirmation field" do
+    user = User.new({
+      email: "name@example.com",
+      name: "Mowgli",
+      password: "junglebook",
+      # password_confirmation: "junglebook"
+    })
+    expect(user).to_not be_valid 
+  end
   it "requires that password and password_confirmation fields match" 
-  it "is valid if a name, unique email, and matching password & password_confirmation fields are provided" 
-  
+  it "is valid if a name, unique email, and matching password & password_confirmation fields are provided" do
+    user = User.new({
+      email: "name@example.com",
+      name: "Mowgli",
+      password: "junglebook",
+      password_confirmation: "junglebook"
+    })
+    expect(user).to be_valid 
+  end
+
 end
