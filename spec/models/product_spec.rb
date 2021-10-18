7c5924744277d4393850ceb6d43705253bbc3632
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-18 18:47:57 -0400

    add specs/tests for validation of name, price, quantity & category in the Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,9 +3,53 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
+    it "is valid if name, price, quantity & category are provided" do
+      category = Category.new()
+      product = category.products.new({
+        name:  'Men\'s Classy shirt',
+        quantity: 10,
+        price: 64.99
+      })
+      expect(product).to be_valid 
+    end
     # validates :name, presence :true
+    it "is not valid without a name" do
+      category = Category.new()
+      product = category.products.new({
+        # name:  'Men\'s Classy shirt',
+        quantity: 10,
+        price: 64.99
+      })
+      expect(product).to_not be_valid 
+    end
     # validates :price, presence :true
+    it "is not valid without a price" do
+      category = Category.new()
+      product = category.products.new({
+        name:  'Men\'s Classy shirt',
+        quantity: 10,
+        # price: 64.99
+      })
+      expect(product).to_not be_valid 
+    end
     # validates :quantity, presence :true
+    it "is not valid without a quantity" do
+      category = Category.new()
+      product = category.products.new({
+        name:  'Men\'s Classy shirt',
+        # quantity: 10,
+        price: 64.99
+      })
+      expect(product).to_not be_valid 
+    end
     # validates :category, presence :true
+    it "is not valid without a category" do
+      product = Product.new({
+        name:  'Men\'s Classy shirt',
+        quantity: 10,
+        price: 64.99
+      })
+      expect(product).to_not be_valid 
+    end
   end
 end
