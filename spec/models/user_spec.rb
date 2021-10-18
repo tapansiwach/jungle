b5d1b3953ab85d72fc81136e79401d7ca78fbc7e
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-18 19:20:45 -0400

    plan data validation tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "requires an email field" 
+  it "ensures that the email is unique, isn't already in the database" 
+  it "requires a name field" 
+  it "requires a password field" 
+  it "requires a password_confirmation field" 
+  it "requires that password and password_confirmation fields match" 
+  it "is valid if a name, unique email, and matching password & password_confirmation fields are provided" 
+  
 end
